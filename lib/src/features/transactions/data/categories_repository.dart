e508e6d0dcfb32caad2cbd8b2e79d4090f3766e8
Jacob Deepu <d2jdeepu@gmail.com>
2026-01-11 17:54:26 +0530

    Implement Categories Repository
        - Create CategoriesRepository to fetch all categories from Drift DB
        - Add Riverpod provider for fetching categories list

---
+++ lib/src/features/transactions/data/categories_repository.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../data/local/database.dart';
+import 'transactions_repository.dart'; // To access databaseProvider
+
+class CategoriesRepository {
+  final AppDatabase _db;
+
+  CategoriesRepository(this._db);
+
+  Future<List<Category>> getAllCategories() {
+    return _db.select(_db.categories).get();
+  }
+}
+
+final categoriesRepositoryProvider = Provider<CategoriesRepository>((ref) {
+  final db = ref.watch(databaseProvider);
+  return CategoriesRepository(db);
+});
+
+final categoriesListProvider = FutureProvider<List<Category>>((ref) async {
+  final repository = ref.watch(categoriesRepositoryProvider);
+  return repository.getAllCategories();
+});
